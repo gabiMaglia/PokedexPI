1bab5325724036e98fe3ce25a8769e5873893586
Gabriel Maglia <gab.maglia@gmail.com>
2023-10-27 22:25:18 -0300

    getAllUsers route implemented

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -1,15 +1,37 @@
+const { User, UserCredentials, PersonalDex } = require("../db");
 
+const getUserHandler = async (req, res) => {
+  try {
+    const allUsers = await User.findAll();
+    res.status(200).json(allUsers);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
 
-const getUserHandler = (req, res) => {}
-const getByIdUserHandler = (req, res) => {}
-const postUserHandler = (req, res) => {}
-const editUserHandler = (req, res) => {}
-
-
+const getByIdUserHandler = async (req, res) => {
+  try {
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+const postUserHandler = async (req, res) => {
+  try {
+    const {first_name, last_name, user_birthdate, user_email, user_handle, user_password } = req.body
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+const editUserHandler = async (req, res) => {
+  try {
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
 
 module.exports = {
-    getUserHandler,
-    getByIdUserHandler,
-    postUserHandler,
-    editUserHandler
-}
+  getUserHandler,
+  getByIdUserHandler,
+  postUserHandler,
+  editUserHandler,
+};
--- api/src/routes/userRoutes/userRoutes.js
+++ api/src/routes/userRoutes/userRoutes.js
@@ -1,11 +1,11 @@
 const {Router} = require('express');
-
+const {getUserHandler, getByIdUserHandler, postUserHandler, editUserHandler} = require('../../controllers/UserController')
 
 const userRouter = Router()
 
-// userRouter.get('/', getUserHandler)
-// userRouter.get('/:id', getByIdUserHandler)
-// userRouter.post('/postnewuser', postUserHandler)
-// userRouter.put('/edituser', editUserHandler)
+userRouter.get('/', getUserHandler)
+userRouter.get('/:id', getByIdUserHandler)
+userRouter.post('/postnewuser', postUserHandler)
+userRouter.put('/edituser', editUserHandler)
 
 module.exports = userRouter
