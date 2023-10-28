c6c288d001c397e5a48e562cd7c7813c08ace22e
Gabriel Maglia <gab.maglia@gmail.com>
2023-10-28 01:09:57 -0300

    controlador postNewUser implementado y funcioando correctamente

--- api/src/controllers/UserController.js
+++ api/src/controllers/UserController.js
@@ -16,36 +16,37 @@ const getByIdUserHandler = async (req, res) => {
   }
 };
 const postUserHandler = async (req, res) => {
-
   try {
-    const {first_name, last_name, user_birthdate, user_email, user_handle, user_password } = req.body
-     // Crear un nuevo usuario
-     const newUser = await User.create({
-        first_name,
-        last_name,
-        user_birthdate,
-        user_email,
-      });
-  
-      // Crear un nuevo registro en la tabla UserCredentials
-      const newUserCredentials = await newUser.createUserCredential({
-        user_handle,
-        user_password,
-      });
-  
-      // Asignar el ID del usuario principal al registro de UserCredentials
-      newUserCredentials.user_id = newUser.id;
-  
-      // Guardar el registro de UserCredentials
-      await newUserCredentials.save();
-  
-      return res.status(200).json(newUser);
+    const {
+      first_name,
+      last_name,
+      user_birthdate,
+      user_email,
+      user_handle,
+      user_password,
+    } = req.body;
 
+    const newUser = await User.create({
+      first_name,
+      last_name,
+      user_birthdate,
+      user_email,
+    });
+
+    const newUserCredentials = await newUser.createUserCredential({
+      user_handle,
+      user_password,
+    });
+
+    newUserCredentials.user_id = newUser.id;
+
+    await newUserCredentials.save();
+
+    return res.status(200).json(newUser);
   } catch (error) {
     return res.status(500).json(error.message);
   }
 };
-
 
 const editUserHandler = async (req, res) => {
   try {
