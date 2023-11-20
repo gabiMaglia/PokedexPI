48f8cf873fc0a1cbd862b5ebc5f5a2768612719a
Gabriel Maglia <gab.maglia@gmail.com>
2023-11-20 16:09:32 -0300

    fdsf

--- client/src/services/pokemonServices.js
+++ client/src/services/pokemonServices.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 const apiUrl = import.meta.env.VITE_API_URL
-
+console.log(apiUrl)
 export const editPokemonService = async (id, data) => {
   try {
     const response = axios;
