8666430d797b3456d1ee9e310fdeb0c8dc51d150
Gabriel Maglia <gab.maglia@gmail.com>
2023-11-20 16:19:13 -0300

    actualizado cors

--- api/src/app.js
+++ api/src/app.js
@@ -3,27 +3,28 @@ const cookieParser = require("cookie-parser");
 const bodyParser = require("body-parser");
 const morgan = require("morgan");
 const routes = require("./routes/mainRoutes.js");
-
+var cors = require('cors')
 require("./db.js");
 
 const server = express();
 
+server.use(cors())
 server.name = "API";
 server.use(morgan("dev"));
 server.use(express.json());
 server.use(bodyParser.urlencoded({ extended: true, limit: "50mb" }));
 server.use(bodyParser.json({ limit: "50mb" }));
 server.use(cookieParser());
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "https://pokedekpi.atlasair.tech/"); // update to match the domain you will make the request from
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "https://pokedekpi.atlasair.tech/"); // update to match the domain you will make the request from
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 server.use("/", routes);
 
